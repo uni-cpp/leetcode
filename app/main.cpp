995e2bbe005351338656e9dee6646fa4e2da9f95
Sergey Polyakov <white.irbys@gmail.com>
2022-05-03 18:26:31 -0700

    162. Find Peak Element

--- app/main.cpp
+++ app/main.cpp
@@ -1,4 +1,5 @@
 #include <leetcode/Solution153.hpp>
+#include <leetcode/Solution162.hpp>
 
 #include <iostream>
 
@@ -10,7 +11,7 @@ main( int /*argc*/, char** /*argv*/ ) -> int
         std::vector< int16_t > in1{ 3, 4, 5, 1, 2 };
         if( solution.findMin( in1 ) != 1 )
         {
-            std::cerr << "Example 1 not passed" << std::endl;
+            std::cerr << "Example 153-1 not passed" << std::endl;
         }
     }
 
@@ -19,7 +20,7 @@ main( int /*argc*/, char** /*argv*/ ) -> int
         std::vector< int16_t > in2{ 4, 5, 6, 7, 0, 1, 2 };
         if( solution.findMin( in2 ) != 0 )
         {
-            std::cerr << "Example 2 not passed" << std::endl;
+            std::cerr << "Example 153-2 not passed" << std::endl;
         }
     }
 
@@ -28,16 +29,61 @@ main( int /*argc*/, char** /*argv*/ ) -> int
         std::vector< int16_t > in3{ 11, 13, 15, 17 };
         if( solution.findMin( in3 ) != 11 )
         {
-            std::cerr << "Example 3 not passed" << std::endl;
+            std::cerr << "Example 153-3 not passed" << std::endl;
         }
     }
 
     {
         leetcode::Solution153 solution;
-        std::vector< int16_t > in3{ 14 };
-        if( solution.findMin( in3 ) != 14 )
+        std::vector< int16_t > in4{ 11 };
+        if( solution.findMin( in4 ) != 11 )
         {
-            std::cerr << "Example 4 not passed" << std::endl;
+            std::cerr << "Example 153-4 not passed" << std::endl;
+        }
+    }
+
+    {
+        leetcode::Solution162 solution;
+        std::vector< int32_t > in1{ 1, 2, 3, 1 };
+        if( solution.findPeakElement( in1 ) != 2 )
+        {
+            std::cerr << "Example 162-1 not passed" << std::endl;
+        }
+    }
+
+    {
+        leetcode::Solution162 solution;
+        std::vector< int32_t > in2{ 1, 2, 1, 3, 5, 6, 4 };
+        if( solution.findPeakElement( in2 ) != 1 && solution.findPeakElement( in2 ) != 5 )
+        {
+            std::cerr << "Example 162-2 not passed" << std::endl;
+        }
+    }
+
+    {
+        leetcode::Solution162 solution;
+        std::vector< int32_t > in3{ 1, 2, 3 };
+        if( solution.findPeakElement( in3 ) != 2 )
+        {
+            std::cerr << "Example 162-3 not passed" << std::endl;
+        }
+    }
+
+    {
+        leetcode::Solution162 solution;
+        std::vector< int32_t > in4{ 3, 2, 3 };
+        if( solution.findPeakElement( in4 ) != 0 && solution.findPeakElement( in4 ) != 2 )
+        {
+            std::cerr << "Example 162-4 not passed" << std::endl;
+        }
+    }
+
+    {
+        leetcode::Solution162 solution;
+        std::vector< int32_t > in5{ 6, 5, 4, 3, 2, 3, 2 };
+        if( solution.findPeakElement( in5 ) != 0 && solution.findPeakElement( in5 ) != 5 )
+        {
+            std::cerr << "Example 162-5 not passed" << std::endl;
         }
     }
 
---
+++ src/leetcode/Solution162.cpp
@@ -0,0 +1,54 @@
+#include <leetcode/Solution162.hpp>
+
+//   /
+//  /
+// /
+
+// \
+//  \
+//   \
+
+//
+//     /\
+//    /  \
+//   /    \
+//  /      \
+// /
+
+// if (num[i] > num[i+1]) => return num[i]
+
+
+// 3-2-3                    0||2
+// 6, 5, 4, 3, 2, 3, 2      0||5
+namespace leetcode
+{
+int32_t
+leetcode::Solution162::findPeakElement( std::vector< int32_t >& nums )
+{
+    if( nums.size( ) < 2 )
+    {
+        return 0;
+    }
+
+    int32_t left = 0;
+    int32_t right = nums.size( ) - 1;
+
+    while( left < right )
+    {
+        int32_t mid = ( left + right ) / 2;
+
+
+        if( nums[ mid ] > nums[ mid + 1 ] )
+        {
+            right = mid;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+}
+
+}  // namespace leetcode
